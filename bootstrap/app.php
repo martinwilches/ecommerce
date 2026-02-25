16ee89eab3c7d491f50acb299a59b5032b663343
Martin Wilches <martin.devsoft@gmail.com>
2026-02-24 22:19:30 -0500

    Ruta admin personalizada

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,6 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function() { // registrar un nuevo archivo de ruta de tipo web
+            Route::middleware('web', 'auth') // el middleware auth permite el acceso a la ruta solo para usuarios autenticados
+                ->prefix('admin') // prefijo de las rutas
+                ->name('admin.') // especificar el prefijo del nombre de las rutas
+                ->group(base_path('routes/admin.php')); // ruta donde se localiza el archivo de rutas
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
